ece2a7d2b9057855ad94389934cc32f618bf1807
BartP <8362400+bpilarczyk@users.noreply.github.com>
2024-11-01 14:58:20 -0400

    Add getHumanChoice function and declare global score variables

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 console.log('Hello and welcome to the amazing Rock, Paper, and Scissors game!');
 
+// declare global variables to store humanScore and computerScore
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
-    // Return an int in the closed range 0, 1, 2
-    let randomNumber = Math.floor(Math.random() * 3);
     // Declare a variable to store computer 'choice'
     let computerChoice;
-    // Assign numeric values to rock, paper, and scissors
+
+    // Return an int in the closed range 0, 1, 2
+    let randomNumber = Math.floor(Math.random() * 3);
     
+    // Assign numeric values to rock, paper, and scissors
     if (randomNumber === 0) {
         computerChoice = 'rock';
     } else if (randomNumber === 1) {
@@ -18,5 +23,28 @@ function getComputerChoice(){
     return computerChoice;
 }
 
-let result = getComputerChoice();
-console.log(result);
+function getHumanChoice(){
+    // let input = prompt("What's your choice?");
+    let input;
+    let humanChoice;
+    
+    while (true) {
+        input = prompt("What's your choice?");
+        humanChoice = input.toLowerCase();
+
+        if (humanChoice === 'rock' || humanChoice === 'paper' || humanChoice === 'scissors') {
+            break;
+        }
+
+    }
+    
+    return humanChoice;
+}
+
+let computerResult = getComputerChoice();
+console.log("Computer's choice: ");
+console.log(computerResult);
+
+let humanResult = getHumanChoice();
+console.log("Human's choice: ");
+console.log(humanResult);
